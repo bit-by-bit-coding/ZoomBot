ca0ecf90b33ce5ee53e56ea0e2a8cc00785b2496
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-03 12:17:09 -0700

    add send_keys_over_time helper function

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,4 @@
+import time
 """
 Classes to hold the most common types of errors
 """
@@ -12,3 +13,14 @@ class RoomIndexNotFoundException(Exception):
 
     def __init__(self, msg):
         print(msg)
+
+
+def send_keys_over_time(element, message, ms_per_key=50, ms_total=None):
+        sec_per_key = ms_per_key if ms_total is None else ms_total/len(message)
+        sec_per_key = sec_per_key/1000
+
+        for character in message:
+            element.send_keys(character)
+            time.sleep(sec_per_key)
+        
+        return element
